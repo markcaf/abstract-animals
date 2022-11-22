eb2c68832166b6d753cfea5d77bd336d0ce291e0
Marco Cafasso <marcocafassog@gmail.com>
2022-11-22 17:06:11 +0100

    feat(Animal): add methods to abstract class
    - void dormi(): show “Zzz” on screen
    - abstract void verso(): shows the call made by the specific animal on the screen
    - abstract void mangia(): show on screen what it eats: grass, meat, ...?

--- JavaAbstract/src/org/abstracts/animals/Animal.java
+++ JavaAbstract/src/org/abstracts/animals/Animal.java
@@ -1,5 +1,12 @@
 package org.abstracts.animals;
 
 public abstract class Animal {
-
+	
+	public void dormi() {
+		System.out.println("Zzzzzzzz");
+	}
+	
+	public abstract void verso();
+	
+	public abstract void mangia();
 }
