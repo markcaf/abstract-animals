f9ae3ec44a79a97c4fc8bf48e67abefc23539839
Marco Cafasso <marcocafassog@gmail.com>
2022-11-22 17:13:39 +0100

    feat(Animale): add constructor, getter and setter

--- JavaAbstract/src/org/abstracts/animals/Animale.java
+++ JavaAbstract/src/org/abstracts/animals/Animale.java
@@ -2,6 +2,19 @@ package org.abstracts.animals;
 
 public abstract class Animale {
 	
+	private String nome;
+	
+	public Animale(String nome) {
+		setNome(nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public void dormi() {
 		System.out.println("Zzzzzzzz");
 	}
